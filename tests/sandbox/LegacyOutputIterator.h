7ec238e476bfff155cd1d60b16de7670ccafc80d
Andrey Romanov <andrey.romanov@auriga.com>
2022-05-23 06:37:30 +0000

    Split code that can be disabled with constexpr for more flexibility

--- tests/sandbox/LegacyOutputIterator.h
+++ tests/sandbox/LegacyOutputIterator.h
@@ -27,52 +27,63 @@ class legacy_output_iterator_requirement : requirement_verifier {
       verify(legacy_iterator_requirement<It>{}.check(type_name), true);
     }
 
+    constexpr bool is_dereferenceable = type_traits::is_dereferenceable_v<It>;
+    constexpr bool can_pre_increment = type_traits::can_pre_increment_v<It>;
+    constexpr bool can_post_increment = type_traits::can_post_increment_v<It>;
+    constexpr bool has_value_type_member =
+        type_traits::has_field::value_type_v<It>;
+
     {
       INFO("Iterator have to be dereferenceable");
-      verify(type_traits::is_dereferenceable_v<It>);
+      verify(is_dereferenceable);
     }
     {
       INFO("Iterator have to implement prefix and postfix increment operator");
-      verify(type_traits::can_pre_increment_v<It>);
-      verify(type_traits::can_post_increment_v<It>);
+      verify(can_pre_increment);
+      verify(can_post_increment);
+    }
+    {
+      INFO("Iterator have to implement std::iterator_traits<It>::value_type");
+      verify(has_value_type_member);
     }
 
-    constexpr bool precondition = type_traits::is_dereferenceable_v<It> &&
-                                  type_traits::can_pre_increment_v<It> &&
-                                  type_traits::can_post_increment_v<It>;
+    using it_traits = std::iterator_traits<It>;
 
-    if constexpr (precondition) {
-      using value_t = typename std::iterator_traits<It>::value_type;
+    if constexpr (has_value_type_member && is_dereferenceable) {
+      INFO("Dereferenced iterator have to be assignble with value_t");
+      verify(std::is_assignable_v<decltype(*std::declval<It>()),
+                                  typename it_traits::value_type>);
+    }
 
-      {
-        INFO("Dereferenced iterator have to be assignble with value_t");
-        verify(std::is_assignable_v<decltype(*std::declval<It>()), value_t>);
-      }
-
+    if constexpr (can_pre_increment) {
       {
         INFO(
-            "Iterator have to return reference after usage of prefix increment "
+            "Iterator have to return reference after usage of prefix "
+            "increment "
             "operator");
         verify(std::is_same_v<decltype(++std::declval<It>()), It &>);
       }
-
       {
         INFO("Iterator have to be convertble to const It after increment");
         verify(std::is_convertible_v<decltype(++std::declval<It>()), const It>);
       }
-
-      {
-        INFO(
-            "Iterator have to be assignable with value_t after increment and "
-            "dereferencing");
-        verify(
-            std::is_assignable_v<decltype(*(std::declval<It>()++)), value_t>);
-      }
-      {
-        INFO("Iterator have to be assignable with value_t after dereferencing");
-        verify(std::is_assignable_v<decltype(*std::declval<It>()), value_t>);
-      }
     }
+
+    if constexpr (can_post_increment && is_dereferenceable &&
+                  has_value_type_member) {
+      INFO(
+          "Iterator have to be assignable with value_t after increment and "
+          "dereferencing");
+      verify(std::is_assignable_v<decltype(*(std::declval<It>()++)),
+                                  typename it_traits::value_type>);
+    }
+
+    if constexpr (is_dereferenceable && has_value_type_member) {
+      INFO("Iterator have to be assignable with value_t after dereferencing");
+      verify(std::is_assignable_v<decltype(*std::declval<It>()),
+                                  typename it_traits::value_type>);
+    }
+
     return _verification_result;
   }
 };
