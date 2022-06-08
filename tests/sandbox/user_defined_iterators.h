3692d3f931173324ec41d829dd263d8cbff8dc91
Andrey Romanov <andrey.romanov@auriga.com>
2022-06-08 09:57:59 +0000

    Minor fixes to stub iterators classes

--- tests/sandbox/user_defined_iterators.h
+++ tests/sandbox/user_defined_iterators.h
@@ -1,14 +1,14 @@
 #pragma once
+#include <initializer_list>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 struct LegacyIterator {
   std::vector<int> v;
   size_t index = 0;
   LegacyIterator(std::initializer_list<int> l) : v(l){};
   LegacyIterator& operator++() {
-    index++;
-    return *this;
-  };
-  LegacyIterator& operator++(int) {
     index++;
     return *this;
   };
@@ -50,9 +50,6 @@ struct LegacyOutputIterator {
     return *this;
   };
 
-  friend bool operator!=(LegacyOutputIterator a, LegacyOutputIterator b) {
-    return a.index != b.index;
-  }
   friend bool operator==(LegacyOutputIterator a, LegacyOutputIterator b) {
     return a.index == b.index;
   }
@@ -71,59 +68,22 @@ class LegacyForwardIterator {
 
   LegacyForwardIterator() : v(), i(0) {}
   LegacyForwardIterator(std::initializer_list<int> l) : v(l){};
-  // Default Copy/Move Are Fine.
-  // Default Destructor fine.
 
   reference operator*() { return v[i]; }
-  pointer operator->() { return &(v[i]); }
-  reference operator[](int m) { return v[i + m]; }
 
   LegacyForwardIterator& operator++() {
     ++i;
     return *this;
   }
-  LegacyForwardIterator& operator--() {
-    --i;
-    return *this;
-  }
+
   LegacyForwardIterator operator++(int) {
     LegacyForwardIterator r(*this);
     ++i;
     return r;
   }
-  LegacyForwardIterator operator--(int) {
-    LegacyForwardIterator r(*this);
-    --i;
-    return r;
-  }
 
-  LegacyForwardIterator& operator+=(int n) {
-    i += n;
-    return *this;
-  }
-  LegacyForwardIterator& operator-=(int n) {
-    i -= n;
-    return *this;
-  }
-
-  LegacyForwardIterator operator+(int n) const {
-    LegacyForwardIterator r(*this);
-    return r += n;
-  }
-  LegacyForwardIterator operator-(int n) const {
-    LegacyForwardIterator r(*this);
-    return r -= n;
-  }
-
-  difference_type operator-(LegacyForwardIterator const& r) const {
-    return i - r.i;
-  }
   size_t size() { return 5; }
 
-  bool operator<(LegacyForwardIterator const& r) const { return i < r.i; }
-  bool operator<=(LegacyForwardIterator const& r) const { return i <= r.i; }
-  bool operator>(LegacyForwardIterator const& r) const { return i > r.i; }
-  bool operator>=(LegacyForwardIterator const& r) const { return i >= r.i; }
   bool operator!=(const LegacyForwardIterator& r) const { return i != r.i; }
   bool operator==(const LegacyForwardIterator& r) const { return i == r.i; }
 
@@ -168,41 +128,8 @@ class LegacyBidirectionalIterator {
     return r;
   }
 
-  LegacyBidirectionalIterator& operator+=(int n) {
-    i += n;
-    return *this;
-  }
-  LegacyBidirectionalIterator& operator-=(int n) {
-    i -= n;
-    return *this;
-  }
-
-  LegacyBidirectionalIterator operator+(int n) const {
-    LegacyBidirectionalIterator r(*this);
-    return r += n;
-  }
-  LegacyBidirectionalIterator operator-(int n) const {
-    LegacyBidirectionalIterator r(*this);
-    return r -= n;
-  }
-
-  difference_type operator-(LegacyBidirectionalIterator const& r) const {
-    return i - r.i;
-  }
-
   size_t size() { return v.size(); }
 
-  // Note: comparing iterator from different containers
-  //       is undefined behavior so we don't need to check
-  //       if they are the same container.
-  bool operator<(LegacyBidirectionalIterator const& r) const { return i < r.i; }
-  bool operator<=(LegacyBidirectionalIterator const& r) const {
-    return i <= r.i;
-  }
-  bool operator>(LegacyBidirectionalIterator const& r) const { return i > r.i; }
-  bool operator>=(LegacyBidirectionalIterator const& r) const {
-    return i >= r.i;
-  }
   bool operator!=(const LegacyBidirectionalIterator& r) const {
     return i != r.i;
   }
@@ -215,51 +142,6 @@ class LegacyBidirectionalIterator {
   int i = 0;
 };
 
-template <>
-struct std::iterator_traits<LegacyBidirectionalIterator> {
-  using value_type = int;
-  using difference_type = int;
-  using pointer = int*;
-  using reference = int&;
-};
-
-template <>
-struct std::iterator_traits<LegacyOutputIterator> {
-  using value_type = int;
-  using difference_type = int;
-  using pointer = int*;
-  using reference = int&;
-};
-
-template <>
-struct std::iterator_traits<LegacyIterator> {
-  using value_type = int;
-  using difference_type = int;
-  using pointer = int*;
-  using reference = int&;
-};
-
-template <>
-struct std::iterator_traits<LegacyInputIterator> {
-  using value_type = int;
-  using difference_type = int;
-  using pointer = int*;
-  using reference = int&;
-};
-
-class LegacyRandomAccessIterator;
-
-template <>
-struct std::iterator_traits<LegacyRandomAccessIterator> {
-  using value_type = int;
-  using difference_type = int;
-  using pointer = int*;
-  using reference = int&;
-};
-
-using diff_t =
-    typename std::iterator_traits<LegacyRandomAccessIterator>::difference_type;
-
 class LegacyRandomAccessIterator {
  public:
   using T = int;
@@ -270,7 +152,7 @@ class LegacyRandomAccessIterator {
   using difference_type = std::ptrdiff_t;
   using iterator_category = std::random_access_iterator_tag;
 
-  LegacyRandomAccessIterator() : v(), i(0) {}
+  LegacyRandomAccessIterator() : i(0), v() {}
   LegacyRandomAccessIterator(std::initializer_list<int> l) : v(l){};
 
   reference operator*() { return v[i]; }
@@ -286,34 +168,28 @@ class LegacyRandomAccessIterator {
     return *this;
   }
   LegacyRandomAccessIterator operator++(int) {
-    LegacyRandomAccessIterator r(*this);
     ++i;
-    return r;
+    return *this;
   }
-  LegacyRandomAccessIterator operator--(int) {
-    LegacyRandomAccessIterator r(*this);
+  LegacyRandomAccessIterator& operator--(int) {
     --i;
-    return r;
+    return *this;
   }
 
-  LegacyRandomAccessIterator& operator+=(
-      typename std::iterator_traits<LegacyRandomAccessIterator>::difference_type
-          n) {
+  LegacyRandomAccessIterator& operator+=(difference_type n) {
     i += n;
     return *this;
   }
-  LegacyRandomAccessIterator& operator-=(
-      typename std::iterator_traits<LegacyRandomAccessIterator>::difference_type
-          n) {
+  LegacyRandomAccessIterator& operator-=(difference_type n) {
     i -= n;
     return *this;
   }
 
-  LegacyRandomAccessIterator operator+(diff_t n) const {
+  LegacyRandomAccessIterator operator+(difference_type n) const {
     LegacyRandomAccessIterator r(*this);
     return r += n;
   }
-  LegacyRandomAccessIterator operator-(diff_t n) const {
+  LegacyRandomAccessIterator operator-(difference_type n) const {
     LegacyRandomAccessIterator r(*this);
     return r -= n;
   }
@@ -341,16 +217,54 @@ class LegacyRandomAccessIterator {
   std::vector<int> v{};
 };
 
-LegacyRandomAccessIterator operator+(diff_t n,
+template <>
+struct std::iterator_traits<LegacyIterator> {
+  using value_type = int;
+  using difference_type = int;
+  using pointer = int*;
+  using reference = int&;
+};
+template <>
+struct std::iterator_traits<LegacyOutputIterator> {
+  using value_type = int;
+  using difference_type = int;
+  using pointer = int*;
+  using reference = int&;
+};
+template <>
+struct std::iterator_traits<LegacyInputIterator> {
+  using value_type = int;
+  using difference_type = int;
+  using pointer = int*;
+  using reference = int&;
+};
+template <>
+struct std::iterator_traits<LegacyBidirectionalIterator> {
+  using value_type = int;
+  using difference_type = int;
+  using pointer = int*;
+  using reference = int&;
+};
+template <>
+struct std::iterator_traits<LegacyRandomAccessIterator> {
+  using value_type = int;
+  using difference_type = int;
+  using pointer = int*;
+  using reference = int&;
+};
+
+using random_diff_t = typename std::iterator_traits<LegacyRandomAccessIterator>::difference_type;
+LegacyRandomAccessIterator operator+(random_diff_t n,
                                      LegacyRandomAccessIterator iterator) {
   LegacyRandomAccessIterator temp = iterator;
   return iterator += n;
 }
-LegacyRandomAccessIterator operator-(diff_t n,
+LegacyRandomAccessIterator operator-(random_diff_t n,
                                      LegacyRandomAccessIterator iterator) {
   LegacyRandomAccessIterator temp = iterator;
   return iterator -= n;
 }
-diff_t operator-(LegacyRandomAccessIterator a, LegacyRandomAccessIterator b) {
+random_diff_t operator-(LegacyRandomAccessIterator a,
+                        LegacyRandomAccessIterator b) {
   return a.i - b.i;
 }
